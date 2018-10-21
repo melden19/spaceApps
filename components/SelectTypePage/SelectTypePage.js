242f116fac6e826df36fd09323228e710598e382
GalaxyPushkin <sashkazar@gmail.com>
2018-10-21 10:45:12 +0300

    dobavil teni na sos

--- components/SelectTypePage/SelectTypePage.js
+++ components/SelectTypePage/SelectTypePage.js
@@ -190,7 +190,13 @@ const styles = StyleSheet.create({
     width: '40%',
     height: '10%',
     marginTop: 15,
-    borderRadius: 40
+    borderRadius: 40,
+    
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 1,
   },
   sostext: {
     fontSize: 40,
